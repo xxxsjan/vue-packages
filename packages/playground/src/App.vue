eceb0cb29ad40ac710987ba70a162e772bf1412f
xxxsjan <626653354@qq.com>
2024-06-04 11:57:15 +0800

    feat: 更新了@esbuild包的解析链接和版本信息

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -6,11 +6,21 @@ import Waterfall from "./pages/test-waterfall.vue";
 import SlideShow from "@/components/slide-show/index.vue";
 
 import DragList from "@/components/drag-list/index.vue";
-
 </script>
 
 <template>
-  <DragList />
+  <div>
+    <h1>TestIp</h1>
+    <TestIp />
+    <!-- <h1>AutoScalingContainer</h1> -->
+    <!-- <TestAutoScalingContainer /> -->
+    <h1>Waterfall</h1>
+    <Waterfall />
+    <h1>SlideShow</h1>
+    <SlideShow />
+    <h1>DragList</h1>
+    <DragList />
+  </div>
 </template>
 
 <style scoped></style>
